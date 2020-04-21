0283b880cd2accc1a627f03c2dec3f5efa19c019
Siddy <sid.cd.varma@gmail.com>
2020-04-21 23:38:00 +0530

    modal added

--- src/components/BrowseTalent.js
+++ src/components/BrowseTalent.js
@@ -14,7 +14,7 @@ const BrowseTalent=()=>{
             <div className=" title-box">
                 <div className="row">
                     <div className="col s4 float-left">
-                        <button className="btn">Add Yourself</button>
+                        <a className="btn modal-trigger" href="#modal1">Add Yourself</a>
                     </div>
                     <div className="col s4 center-align">
                         <h5 className="">Talent Affected by Covid19</h5>
@@ -79,6 +79,15 @@ const BrowseTalent=()=>{
       </table>
             </div>
 
+            <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Modal Header</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div>
         </div>
     )
 }
