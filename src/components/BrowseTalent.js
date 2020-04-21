d1e67260817daff640552c6ee34c2bbae93fd341
Siddy <sid.cd.varma@gmail.com>
2020-04-22 02:21:48 +0530

    add-talent form added

--- src/components/BrowseTalent.js
+++ src/components/BrowseTalent.js
@@ -14,7 +14,7 @@ const BrowseTalent=()=>{
             <div className=" title-box">
                 <div className="row">
                     <div className="col m4 s12 float-left">
-                        <a className="btn modal-trigger" href="#modal1">Add Yourself</a>
+                        <a className="btn modal-trigger" href="#add-talent">Add Yourself</a>
                     </div>
                     <div className="col m4 s12 center-align">
                         <h5 className="">Talent Affected by Covid19</h5>
@@ -23,7 +23,7 @@ const BrowseTalent=()=>{
                     <div class="nav-wrapper">
                             <form>
                                 <div class="input-field">
-                                <input id="search" type="search" placeholder="Search for jobs by sector" required/>
+                                <input id="search" type="search" placeholder="Search for jobs by status" required/>
                                 <label class="label-icon" for="search"><i class="material-icons">search</i>
 
                                 </label>
@@ -47,10 +47,10 @@ const BrowseTalent=()=>{
               <th>I'm Studying/Working</th>
               <th>College Name</th>
               <th>Specialization</th>
-              <th>Company</th>
+              <th>name</th>
+              <th>status</th>
+              <th>Specialization/Title</th>
               <th>Sector</th>
-              <th>Role/Title</th>
-              <th>City</th>
               <th>Open to Relocating?</th>
               <th>Linked URL/Resume</th>
               <th>Email</th>
@@ -79,13 +79,79 @@ const BrowseTalent=()=>{
       </table>
             </div>
 
-            <div id="modal1" class="modal">
+                   <div id="add-talent" class="modal">
     <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
+    <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input  id="name" type="text" className="validate" />
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="status" type="text" className="validate" />
+              <label htmlFor="status">status</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="college-name" type="text" className="validate" />
+              <label htmlFor="college-name">College Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="Specialization" type="password" className="validate" />
+              <label htmlFor="Specialization">Specialization</label>
+            </div>
+          
+            <div className="input-field col s6">
+              <input id="Sector" type="text" className="validate" />
+              <label htmlFor="Sector">Sector</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="Role" type="text" className="validate" />
+              <label htmlFor="Role">Job Posting Link</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="Location" type="text" className="validate" />
+              <label htmlFor="Location">Location</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="relocate" type="text" className="validate" />
+              <label htmlFor="relocate">Open to relocating?</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="resume" type="text" className="validate" />
+              <label htmlFor="resume">LinkedIn URL/Resume</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="email" type="text" className="validate" />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input  id="Phone" type="number" className="validate" />
+              <label htmlFor="Phone">Phone No.</label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
     <div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Submit</a>
     </div>
   </div>
         </div>
