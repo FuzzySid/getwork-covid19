d8dbaad1658385facf5afbb7f0e0f65508d89eb4
Siddy <sid.cd.varma@gmail.com>
2020-04-22 14:20:55 +0530

    logo added on navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -11,7 +11,10 @@ const Navbar=()=>{
       <div className="navbar">
       <nav className="" role="navigation">
           <div className="nav-wrapper container">
-          
+          <a href="#" class="brand-logo left">
+             <img src="https://getwork.org/wp-content/uploads/2019/12/getwork_new.png" width="57px" alt="Jobs &amp; Internships for College Students | GetWork"/>
+  
+          </a>
             <ul className="hide-on-med-and-down right">
                <li><Link  to="/">Home</Link></li>
               <li><Link  to="/browse/talent">Browse Talent</Link></li>
@@ -25,6 +28,17 @@ const Navbar=()=>{
             <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           </div>
         </nav>
+        {/* <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+         */}
       </div>
        
     )
